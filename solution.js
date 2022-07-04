fe2f0ce33f6c88056584bc8e1dec4d63245f3b52
dci-student <alinacuznetov@gmail.com>
2022-07-04 14:49:15 +0200

    string ex

--- solution.js
+++ solution.js
@@ -1,1 +1,54 @@
+/* 1 */
+
+let parkWalkStr = 'I can walk in the park all day!';
+document.write (park);
+
+/* 2 */
+let helloStr = 'Hello World'; 
+console.log(helloStr.toUpperCase());
+document.write (console.log);
+
+/* 3 */
+let earthlingsStr = 'Hello Earthling';
+console.log (earthlingsStr.toLowerCase());
+document.write (console.log);
+
+/* 4 */
+let jsStr = 'JavaScript';
+document.write (aSc);
+
+/* 5 */
+let niceShoesStr = 'nice shoes';
+let result = text.includes("l", "n");
+console.log (result);
+
+/* 6 */
+let oldStr = 'oldshoes';
+let newStr = console.log ('B' + 'oldshoes' + 'B');
+
+/* 7 */
+let scritcherStr = 'awesome';
+let lastThreeChar = console.log (id.slice (id.length -3));
+let result = console.log ('lastThreeChar' + 'stritcherStr' + 'lastThreeChar');
+console.log (result);
+
+/* 8 */
+let boogieNightStr = 'fusion'
+
+/* 9 */
+let firstName = 'Maria';
+let city = 'Berlin';
+let job = 'teacher';
+
+console.log ('My name is' + 'firstName .' + 'I live in' +'city' + 'and I am a' + 'job');
+
+/* 10 */
+let foxStr='the quick brown fox';
+const arr = foxstr.split("");
+for (var i=0; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1); }
+    const foxStr2 = arr.join(" ");
+    console.log(foxStr2);
+
+
 
